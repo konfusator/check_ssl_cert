4cba772f912ca03c853a1c382ae3353449d3bce5
Michael Fischer v. Mollard <github@konfusator.de>
2017-12-11 23:40:55 +0100

    Check SMTP STARTTLS certificates

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ check_ssl_cert
 
 This is a small plugin for icinga/nagios. Its purpose is to check the
 validity of TLS certificates and to warn if they are about to exipire.
-It checks the complete certificate chain. It ist plain go with only
+It checks the complete certificate chain. It is plain go with only
 one non standard package.
 
--- check_ssl_cert.go
+++ check_ssl_cert.go
@@ -18,17 +18,22 @@
 package main
 
 import (
+	"bufio"
 	"crypto/tls"
 	"crypto/x509"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/olorin/nagiosplugin"
+	"net"
+	"os"
+	"strconv"
 	"time"
 )
 
 const (
-	expireSoonFormat = "'%s' (S/N %X) expires in on %s (in %d days)"
-	certOkFormat = "%s (S/N %X) valid until %s (%d days)"
+	expireSoonFormat   = "'%s' (S/N %X) expires in on %s (in %d days)"
+	certOkFormat       = "%s (S/N %X) valid until %s (%d days)"
 	obsAlgorithmFormat = "'%s' (S/N %X) expires after the sunset date for its signature algorithm '%s'."
 )
 
@@ -36,7 +41,6 @@ type sigAlgSunset struct {
 	name      string    // Human readable name of signature algorithm
 	sunsetsAt time.Time // Time the algorithm will be sunset
 }
-
 
 // sunsetSigAlgs is an algorithm to string mapping for signature algorithms
 // which have been or are being deprecated.  See the following links to learn
@@ -72,7 +76,6 @@ type certErrors struct {
 	errs       []error
 }
 
-
 func main() {
 	var (
 		host     string
@@ -80,13 +83,20 @@ func main() {
 		critical int64
 		port     uint
 		timeout  int64
+		proto    string
 	)
-
-	flag.StringVar(&host, "H", "127.0.0.1", "Target host")
-	flag.UintVar(&port, "p", 443, "Target port")
+	flag.StringVar(&host, "host", "127.0.0.1", "Target host")
+	flag.StringVar(&host, "H", "127.0.0.1", "Target host (shorthand)")
+	flag.UintVar(&port, "p", 443, "Target port (shorthand)")
+	flag.UintVar(&port, "port", 443, "Target port")
 	flag.Int64Var(&warn, "w", 32, "days (warning)")
+	flag.Int64Var(&warn, "warn", 32, "days (warning)")
 	flag.Int64Var(&critical, "c", 8, "days (critical)")
+	flag.Int64Var(&critical, "critical", 8, "days (critical)")
+	flag.Int64Var(&timeout, "t", 10, "Plugin timeout")
 	flag.Int64Var(&timeout, "timeout", 10, "Plugin timeout")
+	flag.StringVar(&proto, "proto", "", "Protocol (for STARTTLS). Available: smtp")
+	flag.StringVar(&proto, "P", "", "Protocol (for STARTTLS). Available: smtp")
 	flag.Parse()
 
 	check := nagiosplugin.NewCheck()
@@ -94,12 +104,11 @@ func main() {
 	// If we exit early or panic() we'll still output a result.
 	defer check.Finish()
 
-
 	// now for the timeout I don't really need the return values,
 	// but it looks like something has to be written in the
 	// channel
 	c := make(chan int, 1)
-	go func() { c <- check_ssl_cert(check, host, port, warn, critical) }()
+	go func() { c <- check_ssl_cert(check, host, port, proto, warn, critical) }()
 	select {
 	case <-c:
 		return
@@ -110,25 +119,60 @@ func main() {
 
 }
 
-func check_ssl_cert(check *nagiosplugin.Check, host string, port uint, warn int64, critical int64) int {
+func check_ssl_cert(check *nagiosplugin.Check, host string, port uint, proto string, warn int64, critical int64) int {
 
-	conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", host, port), nil)
-	if err != nil {
-		check.AddResult(nagiosplugin.CRITICAL, fmt.Sprintf("error: %v", err))
-		return int(nagiosplugin.CRITICAL)
+	var conn *tls.Conn
+	if proto == "smtp" {
+		netconn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
+		if err != nil {
+			check.AddResult(nagiosplugin.CRITICAL, fmt.Sprintf("error: %v", err))
+			return int(nagiosplugin.CRITICAL)
+		}
+		defer netconn.Close()
+		reader := bufio.NewReader(netconn)
+		_ , err = readSMTP(reader, 220)
+		me, _ := os.Hostname()
+		fmt.Fprintf(netconn, "EHLO %s\r\n", me)
+		if err != nil {
+			check.AddResult(nagiosplugin.CRITICAL, fmt.Sprintf("Error sending EHLO: %v", err))
+			return int(nagiosplugin.CRITICAL)
+		}
+		msg, err := readSMTP(reader, 250)
+		if err != nil {
+			check.AddResult(nagiosplugin.CRITICAL, fmt.Sprintf("Error reading EHLO response: %s, %v", msg, err))
+			return int(nagiosplugin.CRITICAL)
+		}
+		fmt.Fprintf(netconn, "STARTTLS\r\n")
+		msg, err = readSMTP(reader, 220)
+		if err != nil {
+			check.AddResult(nagiosplugin.CRITICAL, fmt.Sprintf("Error  after STARTTLS: %s, %v", msg, err))
+			return int(nagiosplugin.CRITICAL)
+		}
+		conn = tls.Client(netconn, &tls.Config{ServerName: host})
+		if err = conn.Handshake(); err != nil {
+			check.AddResult(nagiosplugin.CRITICAL, fmt.Sprintf("Error at TLS handshake: %v", err))
+			return int(nagiosplugin.CRITICAL)
+		}
+	} else {
+		tconn, err := tls.Dial("tcp", fmt.Sprintf("%s:%d", host, port), nil)
+		if err != nil {
+			check.AddResult(nagiosplugin.CRITICAL, fmt.Sprintf("error: %v", err))
+			return int(nagiosplugin.CRITICAL)
+		}
+		conn = tconn
 	}
-	defer conn.Close()
 
+	defer conn.Close()
 	timeNow := time.Now()
 
 	for _, chain := range conn.ConnectionState().VerifiedChains {
 		for certNum, cert := range chain {
 
 			expiresIn := int64(cert.NotAfter.Sub(timeNow).Hours())
-			if expiresIn  <= critical * 24  {
+			if expiresIn <= critical*24 {
 				check.AddResult(nagiosplugin.CRITICAL, fmt.Sprintf(expireSoonFormat, cert.Subject.CommonName, cert.SerialNumber, cert.NotAfter, expiresIn/24))
 				return int(nagiosplugin.CRITICAL)
-			} else if expiresIn  <= warn * 24   {
+			} else if expiresIn <= warn*24 {
 				check.AddResult(nagiosplugin.WARNING, fmt.Sprintf(expireSoonFormat, cert.Subject.CommonName, cert.SerialNumber, cert.NotAfter, expiresIn/24))
 				return int(nagiosplugin.WARNING)
 			} else {
@@ -145,4 +189,36 @@ func check_ssl_cert(check *nagiosplugin.Check, host string, port uint, warn int6
 		}
 	}
 	return int(nagiosplugin.UNKNOWN)
+}
+
+func readSMTP(reader *bufio.Reader, expectedCode int) (msg string, err error) {
+
+	line, lerr := reader.ReadString('\n')
+	if lerr != nil {
+		return "", lerr
+	}
+	code, lerr := strconv.Atoi(line[0:3])
+	if lerr != nil {
+		return "", lerr
+	}
+	if code != expectedCode {
+		return "", errors.New("Unexpected answer code")
+	}
+	msg = line[4:]
+	// Multiline
+	for line[3] == '-' {
+		line, lerr = reader.ReadString('\n')
+		if lerr != nil {
+			return "", lerr
+		}
+		loopcode, lerr := strconv.Atoi(line[0:3])
+		if lerr != nil {
+			return "", lerr
+		}
+		if loopcode != code {
+			return line, errors.New("Unexpected answer code in multiline")
+		}
+		msg += line[4:]
+	}
+	return msg, err
 }
